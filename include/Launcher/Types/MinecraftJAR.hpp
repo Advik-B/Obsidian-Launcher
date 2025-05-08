855dae9233abf8870797715096a572026618d746
Advik <advik.b@gmail.com>
2025-05-09 01:02:28 +0530

    Create MinecraftJAR.hpp

---
+++ include/Launcher/Types/MinecraftJAR.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Advik on 09-05-2025.
+//
+
+#ifndef MINECRAFTJAR_HPP
+#define MINECRAFTJAR_HPP
+
+enum class MinecraftJARType {
+    CLIENT = 1,
+    SERVER = 2
+};
+
+#endif //MINECRAFTJAR_HPP
