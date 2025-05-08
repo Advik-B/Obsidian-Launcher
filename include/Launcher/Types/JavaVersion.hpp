d5f36a8dfad00a9b26573b905e65bd5ff38d8df0
Advik <advik.b@gmail.com>
2025-05-09 01:01:01 +0530

    Create JavaVersion.hpp

---
+++ include/Launcher/Types/JavaVersion.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Advik on 09-05-2025.
+//
+
+#ifndef JAVAVERSION_HPP
+#define JAVAVERSION_HPP
+
+#include <string>
+
+struct JavaVersion {
+    std::string component;
+    unsigned int majorVersion;
+};
+
+#endif //JAVAVERSION_HPP
