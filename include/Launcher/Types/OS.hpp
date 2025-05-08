2ee7597d85d490f92ea9f4ccc81ec232aaa6aef3
Advik <advik.b@gmail.com>
2025-05-09 01:02:32 +0530

    Create OS.hpp

---
+++ include/Launcher/Types/OS.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Advik on 09-05-2025.
+//
+
+#ifndef RULEOS_HPP
+#define RULEOS_HPP
+
+#include <string>
+#include <optional>
+
+struct OS {
+    std::string name;
+    std::optional<std::string> version;
+};
+
+#endif //RULEOS_HPP
