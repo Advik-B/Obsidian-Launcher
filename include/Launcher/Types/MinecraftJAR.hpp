f32c2a46ce0c218c0a69560224b72c2669e5ab98
Advik <advik.b@gmail.com>
2025-05-09 01:05:49 +0530

    Update MinecraftJAR.hpp

--- include/Launcher/Types/MinecraftJAR.hpp
+++ include/Launcher/Types/MinecraftJAR.hpp
@@ -4,10 +4,18 @@
 
 #ifndef MINECRAFTJAR_HPP
 #define MINECRAFTJAR_HPP
+#include <string>
 
 enum class MinecraftJARType {
     CLIENT = 1,
     SERVER = 2
+};
+
+struct MinecraftJAR {
+    const MinecraftJARType type;
+    const std::string sha1;
+    const unsigned int size;
+    const std::string url;
 };
 
 #endif //MINECRAFTJAR_HPP
