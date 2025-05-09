b94e8fd5ca59e9067444e93e408532f9cd25c12e
Advik <advik.b@gmail.com>
2025-05-09 19:34:13 +0530

    Update Library.hpp

--- include/Launcher/Types/Library.hpp
+++ include/Launcher/Types/Library.hpp
@@ -6,9 +6,24 @@
 #define LIBRARY_HPP
 
 #include <string>
+#include <Launcher/Types/Rule.hpp>
+#include <unordered_set>
+
+struct LibraryArtifact {
+    const std::string path;
+    const std::string sha1;
+    const unsigned int size;
+    const std::string url;
+};
+
+struct LibraryDownloads {
+    const LibraryArtifact artifact;
+};
 
 struct Library {
     const std::string name;
+    const LibraryDownloads downloads;
+    const std::unordered_set<Rule> rules;
 };
 
 #endif //LIBRARY_HPP
