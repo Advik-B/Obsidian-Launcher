b5833c897911cbf26363a797345a6d824ff15481
Advik <advik.b@gmail.com>
2025-05-15 14:05:26 +0530

    Update Library.hpp

--- include/Launcher/Types/Library.hpp
+++ include/Launcher/Types/Library.hpp
@@ -7,23 +7,43 @@
 
 #include <string>
 #include <Launcher/Types/Rule.hpp>
-#include <unordered_set>
+#include <vector>
+#include <map>
+#include <optional>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 struct LibraryArtifact {
-    const std::string path;
-    const std::string sha1;
-    const unsigned int size;
-    const std::string url;
+    std::string path;
+    std::string sha1;
+    unsigned int size;
+    std::string url;
+
+    static LibraryArtifact from_json(const json& j);
 };
 
 struct LibraryDownloads {
-    const LibraryArtifact artifact;
+    std::optional<LibraryArtifact> artifact;
+    std::map<std::string, LibraryArtifact> classifiers; // Key: e.g., "natives-linux"
+
+    static LibraryDownloads from_json(const json& j);
+};
+
+struct LibraryExtractRule {
+    std::vector<std::string> exclude;
+
+    static LibraryExtractRule from_json(const json& j);
 };
 
 struct Library {
-    const std::string name;
-    const LibraryDownloads downloads;
-    const std::unordered_set<Rule> rules;
+    std::string name;
+    std::optional<LibraryDownloads> downloads;
+    std::vector<Rule> rules;
+    std::map<std::string, std::string> natives; // OS to classifier key e.g. "linux": "natives-linux"
+    std::optional<LibraryExtractRule> extract;
+
+    static Library from_json(const json& j);
 };
 
 #endif //LIBRARY_HPP
