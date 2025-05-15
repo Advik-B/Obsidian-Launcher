889e3056acae33faaaaff654aaaeb7c5c23e4fa2
Advik <advik.b@gmail.com>
2025-05-15 14:05:49 +0530

    Update VersionMeta.cpp

--- src/VersionMeta.cpp
+++ src/VersionMeta.cpp
@@ -1,25 +1,14 @@
 //
 // Created by Advik on 08-05-2025.
 //
-
 #include <Launcher/Types/VersionMeta.hpp>
-#include <chrono>
 
 VersionMeta VersionMeta::from_json(json j) {
-    using namespace std::chrono;
-
-    const auto id = j["id"].get<std::string>();
-    const auto sha1 = j["sha1"].get<std::string>();
-    const auto complianceLevel = j["complianceLevel"].get<unsigned int>();
-
-    const auto timePoint = j["time"].get<std::string>();
-    const auto releaseTimePoint = j["releaseTime"].get<std::string>();
-
-    return VersionMeta{
-        id,
-        releaseTimePoint,
-        timePoint,
-        sha1,
-        complianceLevel
-    };
+    VersionMeta meta;
+    meta.id = j.at("id").get<std::string>();
+    meta.releaseTime = j.at("releaseTime").get<std::string>();
+    meta.time = j.at("time").get<std::string>();
+    meta.sha1 = j.at("sha1").get<std::string>();
+    meta.complianceLevel = j.at("complianceLevel").get<unsigned int>();
+    return meta;
 }
