782172d4b0db40665a97af352bac0a985494b894
Advik <advik.b@gmail.com>
2025-05-15 14:05:29 +0530

    Update MinecraftJAR.hpp

--- include/Launcher/Types/MinecraftJAR.hpp
+++ include/Launcher/Types/MinecraftJAR.hpp
@@ -5,17 +5,26 @@
 #ifndef MINECRAFTJAR_HPP
 #define MINECRAFTJAR_HPP
 #include <string>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 enum class MinecraftJARType {
     CLIENT = 1,
-    SERVER = 2
+    SERVER = 2,
+    CLIENT_MAPPING = 3,
+    SERVER_MAPPING = 4,
 };
 
-struct MinecraftJAR {
-    const MinecraftJARType type;
-    const std::string sha1;
-    const unsigned int size;
-    const std::string url;
+MinecraftJARType string_to_minecraft_jar_type(const std::string& s);
+std::string minecraft_jar_type_to_string(MinecraftJARType type);
+
+struct DownloadDetails {
+    std::string sha1;
+    unsigned int size;
+    std::string url;
+
+    static DownloadDetails from_json(const json& j);
 };
 
 #endif //MINECRAFTJAR_HPP
