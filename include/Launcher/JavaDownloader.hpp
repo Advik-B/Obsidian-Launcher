3ba1a5202b4235cc555298376dc7d645c4ac85e5
Advik <advik.b@gmail.com>
2025-05-26 22:57:44 +0530

    Update JavaDownloader.hpp

--- include/Launcher/JavaDownloader.hpp
+++ include/Launcher/JavaDownloader.hpp
@@ -6,20 +6,23 @@
 #include <Launcher/Types/JavaVersion.hpp>
 #include <Launcher/Types/Version.hpp>
 #include <filesystem>
-#include <spdlog/logger.h> // For std::shared_ptr<spdlog::logger>
-#include <nlohmann/json_fwd.hpp> // Forward declare json
+#include <spdlog/logger.h>
+#include <nlohmann/json_fwd.hpp>
 
 namespace Launcher {
 
+    class HttpManager; // Forward declaration
+
     class JavaDownloader {
     public:
-        JavaDownloader();
+        JavaDownloader(HttpManager& httpManager); // Constructor takes HttpManager
 
         std::filesystem::path downloadJavaForMinecraftVersionMojang(const Version& mcVersion, const std::filesystem::path& baseDownloadDir);
         std::filesystem::path downloadJavaForSpecificVersionAdoptium(const JavaVersion& requiredJava, const std::filesystem::path& baseDownloadDir);
 
     private:
-        nlohmann::json fetchMojangJavaManifest(); // Return type nlohmann::json
+        nlohmann::json fetchMojangJavaManifest();
+        HttpManager& m_httpManager; // Store reference to HttpManager
         std::shared_ptr<spdlog::logger> m_logger;
     };
 
