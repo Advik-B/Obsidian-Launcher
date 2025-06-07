ae40fa3b615dee0064694f725671bc25bdc74d38
Advik <advik.b@gmail.com>
2025-06-07 23:23:08 +0530

    Create AssetObjectInfo.cs

---
+++ Models/AssetObjectInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ObsidianLauncher.Models
+{
+    /// <summary>
+    /// Represents information about a single asset object (e.g., texture, sound file).
+    /// This corresponds to the value part of the "objects" map in an asset index JSON file.
+    /// </summary>
+    public class AssetObjectInfo
+    {
+        /// <summary>
+        /// The SHA1 hash of the asset file. This hash is also used as part of its storage path.
+        /// </summary>
+        [JsonPropertyName("hash")]
+        public string Hash { get; set; }
+
+        /// <summary>
+        /// The size of the asset file in bytes.
+        /// </summary>
+        [JsonPropertyName("size")]
+        public ulong Size { get; set; } // Using ulong for consistency with AssetIndex.Size
+    }
+}
