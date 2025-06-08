d8bac825a8dc4facb2549cbd9abc23c28b7b68d6
Advik <advik.b@gmail.com>
2025-06-08 23:14:47 +0530

    Create LogHelper.cs

---
+++ Utils/LogHelper.cs
@@ -0,0 +1,9 @@
+﻿using Serilog;
+
+namespace ObsidianLauncher.Utils;
+
+public static class LogHelper
+{
+    public static ILogger GetLogger<T>() =>
+        Log.ForContext("SourceContext", typeof(T).Name);
+}
