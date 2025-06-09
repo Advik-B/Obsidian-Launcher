2de0aa6cb4e70f89c14e812de86a6f346f47ae61
Advik <advik.b@gmail.com>
2025-06-09 19:56:41 +0530

    Update VersionArguments.cs

--- Models/VersionArguments.cs
+++ Models/VersionArguments.cs
@@ -1,31 +1,31 @@
 ﻿// Models/VersionArguments.cs
+
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
-namespace ObsidianLauncher.Models
+namespace ObsidianLauncher.Models;
+
+/// <summary>
+///     Contains the game and JVM arguments for a Minecraft version.
+///     Corresponds to the "arguments" object in the Minecraft version manifest.
+/// </summary>
+public class VersionArguments
 {
-    /// <summary>
-    /// Contains the game and JVM arguments for a Minecraft version.
-    /// Corresponds to the "arguments" object in the Minecraft version manifest.
-    /// </summary>
-    public class VersionArguments
+    public VersionArguments()
     {
-        /// <summary>
-        /// List of game arguments. Each element can be a plain string or a conditional argument.
-        /// </summary>
-        [JsonPropertyName("game")]
-        public List<VersionArgument> Game { get; set; }
-
-        /// <summary>
-        /// List of JVM arguments. Each element can be a plain string or a conditional argument.
-        /// </summary>
-        [JsonPropertyName("jvm")]
-        public List<VersionArgument> Jvm { get; set; }
-
-        public VersionArguments()
-        {
-            Game = new List<VersionArgument>();
-            Jvm = new List<VersionArgument>();
-        }
+        Game = new List<VersionArgument>();
+        Jvm = new List<VersionArgument>();
     }
+
+    /// <summary>
+    ///     List of game arguments. Each element can be a plain string or a conditional argument.
+    /// </summary>
+    [JsonPropertyName("game")]
+    public List<VersionArgument> Game { get; set; }
+
+    /// <summary>
+    ///     List of JVM arguments. Each element can be a plain string or a conditional argument.
+    /// </summary>
+    [JsonPropertyName("jvm")]
+    public List<VersionArgument> Jvm { get; set; }
 }
