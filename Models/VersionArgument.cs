6d2f5843d40036fe6471e79fc8e9ea30d3e2f68d
Advik <advik.b@gmail.com>
2025-06-09 19:56:38 +0530

    Update VersionArgument.cs

--- Models/VersionArgument.cs
+++ Models/VersionArgument.cs
@@ -1,77 +1,76 @@
 ﻿// Models/VersionArgument.cs
+
 using System;
-using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace ObsidianLauncher.Models
+namespace ObsidianLauncher.Models;
+
+/// <summary>
+///     Represents a single game or JVM argument, which can be a plain string
+///     or a conditional argument with rules.
+///     This class uses a custom JsonConverter to handle the variant type.
+/// </summary>
+[JsonConverter(typeof(VersionArgumentConverter))]
+public class VersionArgument
 {
-    /// <summary>
-    /// Represents a single game or JVM argument, which can be a plain string
-    /// or a conditional argument with rules.
-    /// This class uses a custom JsonConverter to handle the variant type.
-    /// </summary>
-    [JsonConverter(typeof(VersionArgumentConverter))]
-    public class VersionArgument
+    // Private constructors, only used by the converter
+    private VersionArgument(string plainValue)
     {
-        public string PlainStringValue { get; private set; }
-        public ConditionalArgumentValue ConditionalValue { get; private set; }
-
-        public bool IsPlainString => PlainStringValue != null;
-        public bool IsConditional => ConditionalValue != null;
-
-        // Private constructors, only used by the converter
-        private VersionArgument(string plainValue)
-        {
-            PlainStringValue = plainValue;
-            ConditionalValue = null;
-        }
-
-        private VersionArgument(ConditionalArgumentValue conditionalValue)
-        {
-            PlainStringValue = null;
-            ConditionalValue = conditionalValue;
-        }
-
-        // Static factory methods for the converter to use
-        public static VersionArgument Create(string plainValue) => new VersionArgument(plainValue);
-        public static VersionArgument Create(ConditionalArgumentValue conditionalValue) => new VersionArgument(conditionalValue);
+        PlainStringValue = plainValue;
+        ConditionalValue = null;
     }
 
-    public class VersionArgumentConverter : JsonConverter<VersionArgument>
+    private VersionArgument(ConditionalArgumentValue conditionalValue)
     {
-        public override VersionArgument Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        PlainStringValue = null;
+        ConditionalValue = conditionalValue;
+    }
+
+    public string PlainStringValue { get; }
+    public ConditionalArgumentValue ConditionalValue { get; }
+
+    public bool IsPlainString => PlainStringValue != null;
+    public bool IsConditional => ConditionalValue != null;
+
+    // Static factory methods for the converter to use
+    public static VersionArgument Create(string plainValue)
+    {
+        return new VersionArgument(plainValue);
+    }
+
+    public static VersionArgument Create(ConditionalArgumentValue conditionalValue)
+    {
+        return new VersionArgument(conditionalValue);
+    }
+}
+
+public class VersionArgumentConverter : JsonConverter<VersionArgument>
+{
+    public override VersionArgument Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType == JsonTokenType.String) return VersionArgument.Create(reader.GetString());
+
+        if (reader.TokenType == JsonTokenType.StartObject)
         {
-            if (reader.TokenType == JsonTokenType.String)
-            {
-                return VersionArgument.Create(reader.GetString());
-            }
-            else if (reader.TokenType == JsonTokenType.StartObject)
-            {
-                // Deserialize the object as ConditionalArgumentValue
-                // We need to be careful here not to consume the reader in a way that breaks the outer deserializer
-                // if options are passed that affect how objects are read.
-                // A simpler way is to use JsonSerializer.Deserialize on the current element if possible.
-                var conditionalValue = JsonSerializer.Deserialize<ConditionalArgumentValue>(ref reader, options);
-                return VersionArgument.Create(conditionalValue);
-            }
-            throw new JsonException("Expected string or object for VersionArgument");
+            // Deserialize the object as ConditionalArgumentValue
+            // We need to be careful here not to consume the reader in a way that breaks the outer deserializer
+            // if options are passed that affect how objects are read.
+            // A simpler way is to use JsonSerializer.Deserialize on the current element if possible.
+            var conditionalValue = JsonSerializer.Deserialize<ConditionalArgumentValue>(ref reader, options);
+            return VersionArgument.Create(conditionalValue);
         }
 
-        public override void Write(Utf8JsonWriter writer, VersionArgument value, JsonSerializerOptions options)
-        {
-            if (value.IsPlainString)
-            {
-                writer.WriteStringValue(value.PlainStringValue);
-            }
-            else if (value.IsConditional)
-            {
-                JsonSerializer.Serialize(writer, value.ConditionalValue, options);
-            }
-            else
-            {
-                writer.WriteNullValue(); // Or throw exception if null is not valid
-            }
-        }
+        throw new JsonException("Expected string or object for VersionArgument");
+    }
+
+    public override void Write(Utf8JsonWriter writer, VersionArgument value, JsonSerializerOptions options)
+    {
+        if (value.IsPlainString)
+            writer.WriteStringValue(value.PlainStringValue);
+        else if (value.IsConditional)
+            JsonSerializer.Serialize(writer, value.ConditionalValue, options);
+        else
+            writer.WriteNullValue(); // Or throw exception if null is not valid
     }
 }
