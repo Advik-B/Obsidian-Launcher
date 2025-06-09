5bbc16d1a881b8f7d8b7c455c1ed238af345e820
Advik <advik.b@gmail.com>
2025-06-09 19:54:59 +0530

    Update ArchitectureType.cs

--- Enums/ArchitectureType.cs
+++ Enums/ArchitectureType.cs
@@ -1,11 +1,10 @@
-﻿namespace ObsidianLauncher.Enums
+﻿namespace ObsidianLauncher.Enums;
+
+public enum ArchitectureType
 {
-    public enum ArchitectureType
-    {
-        X86,    // 32-bit x86
-        X64,    // 64-bit x86_64/amd64
-        Arm,    // 32-bit ARM
-        Arm64,  // 64-bit ARM (aarch64)
-        Unknown
-    }
+    X86, // 32-bit x86
+    X64, // 64-bit x86_64/amd64
+    Arm, // 32-bit ARM
+    Arm64, // 64-bit ARM (aarch64)
+    Unknown
 }
