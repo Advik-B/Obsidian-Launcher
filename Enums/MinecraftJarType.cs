721e0ecd832c68299264614dce9a93d722fb969b
Advik <advik.b@gmail.com>
2025-06-09 19:55:09 +0530

    Update MinecraftJarType.cs

--- Enums/MinecraftJarType.cs
+++ Enums/MinecraftJarType.cs
@@ -1,44 +1,43 @@
 ﻿// Enums/MinecraftJarType.cs
 
-namespace ObsidianLauncher.Enums
+namespace ObsidianLauncher.Enums;
+
+/// <summary>
+///     Represents the different types of downloadable Minecraft JARs or related files
+///     as typically found in the "downloads" section of a version manifest.
+/// </summary>
+public enum MinecraftJarType
 {
     /// <summary>
-    /// Represents the different types of downloadable Minecraft JARs or related files
-    /// as typically found in the "downloads" section of a version manifest.
+    ///     The main Minecraft client JAR file.
+    ///     JSON key: "client"
     /// </summary>
-    public enum MinecraftJarType
-    {
-        /// <summary>
-        /// The main Minecraft client JAR file.
-        /// JSON key: "client"
-        /// </summary>
-        Client,
+    Client,
 
-        /// <summary>
-        /// The Minecraft server JAR file.
-        /// JSON key: "server"
-        /// </summary>
-        Server,
+    /// <summary>
+    ///     The Minecraft server JAR file.
+    ///     JSON key: "server"
+    /// </summary>
+    Server,
 
-        /// <summary>
-        /// The client mappings file (e.g., for ProGuard/Obfuscation).
-        /// JSON key: "client_mappings"
-        /// </summary>
-        ClientMappings,
+    /// <summary>
+    ///     The client mappings file (e.g., for ProGuard/Obfuscation).
+    ///     JSON key: "client_mappings"
+    /// </summary>
+    ClientMappings,
 
-        /// <summary>
-        /// The server mappings file.
-        /// JSON key: "server_mappings"
-        /// </summary>
-        ServerMappings,
+    /// <summary>
+    ///     The server mappings file.
+    ///     JSON key: "server_mappings"
+    /// </summary>
+    ServerMappings,
 
-        // TODO: Add other types here if Mojang introduces them, e.g., "windows_server" for Bedrock Edition related things.
-        // Note: Bedrock Edition manifests are usually separate from Java Edition manifests.
+    // TODO: Add other types here if Mojang introduces them, e.g., "windows_server" for Bedrock Edition related things.
+    // Note: Bedrock Edition manifests are usually separate from Java Edition manifests.
 
-        /// <summary>
-        /// Represents an unknown or unsupported JAR type.
-        /// This can be used as a default or error value.
-        /// </summary>
-        Unknown
-    }
+    /// <summary>
+    ///     Represents an unknown or unsupported JAR type.
+    ///     This can be used as a default or error value.
+    /// </summary>
+    Unknown
 }
