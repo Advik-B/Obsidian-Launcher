08f1637c5f2cea9a30971b01e4c796177d2ffc40
Advik <advik.b@gmail.com>
2025-06-09 19:55:57 +0530

    Update Instance.cs

--- Models/Instance.cs
+++ Models/Instance.cs
@@ -1,43 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO; 
+using System.IO;
 using System.Text.Json.Serialization;
 
-namespace ObsidianLauncher.Models
+namespace ObsidianLauncher.Models;
+
+public class Instance
 {
-    public class Instance
+    public Instance()
     {
-        public string Id { get; set; } 
-        public string Name { get; set; } 
-        public string MinecraftVersionId { get; set; } 
-        public string InstancePath { get; set; } 
-        
-        [JsonIgnore] 
-        public string NativesPath => Path.Combine(InstancePath, "natives");
-        
-        [JsonIgnore] 
-        public string GameDataPath => InstancePath;
-
-        public string CustomJavaRuntimePath { get; set; }
-        public List<string> CustomJvmArguments { get; set; }
-        // PlayerName removed from instance metadata
-
-        public DateTime CreationDate { get; set; }
-        public DateTime LastPlayedDate { get; set; }
-
-        public TimeSpan TotalPlaytime { get; set; }
-        public TimeSpan LastSessionPlaytime { get; set; }
-        
-        public string CustomIconPath { get; set; }
-
-        public Instance()
-        {
-            Id = Guid.NewGuid().ToString();
-            CreationDate = DateTime.UtcNow;
-            LastPlayedDate = DateTime.MinValue; 
-            CustomJvmArguments = new List<string>();
-            TotalPlaytime = TimeSpan.Zero;
-            LastSessionPlaytime = TimeSpan.Zero;
-        }
+        Id = Guid.NewGuid().ToString();
+        CreationDate = DateTime.UtcNow;
+        LastPlayedDate = DateTime.MinValue;
+        CustomJvmArguments = new List<string>();
+        TotalPlaytime = TimeSpan.Zero;
+        LastSessionPlaytime = TimeSpan.Zero;
     }
+
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string MinecraftVersionId { get; set; }
+    public string InstancePath { get; set; }
+
+    [JsonIgnore] public string NativesPath => Path.Combine(InstancePath, "natives");
+
+    [JsonIgnore] public string GameDataPath => InstancePath;
+
+    public string CustomJavaRuntimePath { get; set; }
+
+    public List<string> CustomJvmArguments { get; set; }
+    // PlayerName removed from instance metadata
+
+    public DateTime CreationDate { get; set; }
+    public DateTime LastPlayedDate { get; set; }
+
+    public TimeSpan TotalPlaytime { get; set; }
+    public TimeSpan LastSessionPlaytime { get; set; }
+
+    public string CustomIconPath { get; set; }
 }
