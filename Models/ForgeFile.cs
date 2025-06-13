6087522c59d993f1bebc878613087c0ba012298e
Advik <advik.b@gmail.com>
2025-06-14 00:07:35 +0530

    Create ForgeFile.cs

---
+++ Models/ForgeFile.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ObsidianLauncher.Models;
+
+/// <summary>
+/// Represents a single file within the metadata for a Forge version from a metadata API.
+/// </summary>
+public class ForgeFile
+{
+    [JsonPropertyName("format")]
+    public string Format { get; set; }
+
+    [JsonPropertyName("category")]
+    public string Category { get; set; }
+
+    [JsonPropertyName("hash")]
+    public string Hash { get; set; }
+}
