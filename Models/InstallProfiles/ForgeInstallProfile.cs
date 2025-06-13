b5fe7bae3ed4d4fb9c01b9ddb15e400d8df4e21e
Advik <advik.b@gmail.com>
2025-06-14 00:07:39 +0530

    Create ForgeInstallProfile.cs

---
+++ Models/InstallProfiles/ForgeInstallProfile.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ObsidianLauncher.Models.InstallProfiles;
+
+/// <summary>
+/// Represents the installation profile found within a Forge installer JAR.
+/// This model maps directly to the structure of 'install_profile.json'.
+/// </summary>
+public class ForgeInstallProfile
+{
+    /// <summary>
+    /// Contains the complete version JSON object for the Forge-patched version.
+    /// This is deserialized directly into our existing MinecraftVersion model.
+    /// </summary>
+    [JsonPropertyName("versionInfo")]
+    public MinecraftVersion VersionInfo { get; set; }
+}
